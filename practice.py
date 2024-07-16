c7a8e0585dee9a00e50cb7e7ac2e476e99121b47
Bhanu Teja <bhanu25201209@gmail.com>
2024-07-16 21:09:57 +0530

    backend by python

---
+++ practice.py
@@ -0,0 +1,18 @@
+from flask import Flask 
+from flask import render_template 
+from flask import request
+app=Flask(__name__)
+
+@app.route('/hello', methods=["GET","POST"])
+def hello():
+    if request.method == "GET": 
+       return render_template("hello.html")
+    elif request.method == "POST":
+        a=request.form["your_name"]
+        return render_template("display.html",display_name=a)
+    else:
+        return "error"
+
+if __name__=='__main__':
+    app.debug=True
+    app.run()
