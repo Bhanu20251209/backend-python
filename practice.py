18f3651e7d7abb8168c81e4f1313de0aafaff4a4
Bhanu Teja <bhanu25201209@gmail.com>
2024-07-21 11:05:29 +0530

    git push

--- practice.py
+++ practice.py
@@ -1,18 +1,21 @@
 from flask import Flask 
-from flask import render_template 
+from flask import render_template,redirect,url_for
 from flask import request
 app=Flask(__name__)
 
-@app.route('/hello', methods=["GET","POST"])
+@app.route('/',methods=['GET','POST'])
 def hello():
-    if request.method == "GET": 
-       return render_template("hello.html")
-    elif request.method == "POST":
-        a=request.form["your_name"]
-        return render_template("display.html",display_name=a)
+    if request.method=='GET':
+        return render_template('hello.html')
+    elif request.method=='POST':
+        a=request.form['your_name']
+        return render_template('display.html',display_name=a)
     else:
-        return "error"
-
+        return 'error'
+    
+        
+ 
+ 
 if __name__=='__main__':
     app.debug=True
     app.run()
